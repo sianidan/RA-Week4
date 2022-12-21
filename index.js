a255a5962d1d92bb719814fe41f7d85acff46945
Siani <99415543+sianidan@users.noreply.github.com>
2022-12-20 19:59:45 -0500

    push Simple JS Function

---
+++ index.js
@@ -0,0 +1,12 @@
+/*Write a javascript code that prints out your name, height and country on the screen.
+ 1. Create an index.js file.
+ 2. Write a function that takes three(3) parameters (name, height and country)
+ 3. Write an alert in the function that prints out the inputs.
+ 4. Call the function and pass the three(3) parameter values into it.
+*/
+
+function me(name, height, country) {
+    alert(name + ", " + height + ", " + country);
+}
+
+me('Siani', "5'0", 'United States');
